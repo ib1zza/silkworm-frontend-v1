4ae86ed1a033049ec3444f8eada4a6ce713136e0
ib1zza <dremast1337@gmail.com>
2026-01-14 19:43:33 +0300

    feat: implement animated DVD component and enhance hero section layout

--- src/pages/IndexPage/IndexPage.tsx
+++ src/pages/IndexPage/IndexPage.tsx
@@ -1,10 +1,62 @@
-import { Catalog } from "@/widgets/Catalog";
+import { useEffect, useRef } from "react";
 import s from "./IndexPage.module.scss";
+import LogoIcon from "@/shared/assets/vector/preloader.svg?react";
+import { Catalog } from "@/widgets/Catalog";
 
 export default function IndexPage() {
+  const dvdRef = useRef<HTMLDivElement>(null);
+  const heroRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const dvd = dvdRef.current;
+    const hero = heroRef.current;
+
+    if (!dvd || !hero) return;
+
+    let x = 50;
+    let y = 50;
+    let vx = 1.2;
+    let vy = 1.2;
+
+    const animate = () => {
+      const heroRect = hero.getBoundingClientRect();
+      const dvdRect = dvd.getBoundingClientRect();
+
+      x += vx;
+      y += vy;
+
+      if (x + dvdRect.width >= heroRect.width || x <= 0) {
+        vx *= -1;
+      }
+
+      if (y + dvdRect.height >= heroRect.height || y <= 0) {
+        vy *= -1;
+      }
+
+      dvd.style.transform = `translate3d(${x}px, ${y}px, 0)`;
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, []);
+
+  const handleClick = () => {
+    const dvd = dvdRef.current;
+    if (!dvd) return;
+
+    dvd.classList.remove(s.flip);
+    // перезапуск анимации
+    void dvd.offsetWidth;
+    dvd.classList.add(s.flip);
+  };
+
   return (
     <div className={s.page}>
-      <div className={s.hero}></div>
+      <div className={s.hero} ref={heroRef}>
+        <div className={s.dvd} ref={dvdRef} onClick={handleClick}>
+          <LogoIcon />
+        </div>
+      </div>
 
       <Catalog />
     </div>
--- src/shared/ui/Preloader/Preloader.tsx
+++ src/shared/ui/Preloader/Preloader.tsx
@@ -5,6 +5,5 @@ import LogoIcon from "@/shared/assets/vector/preloader.svg?react";
 export const Preloader = () => (
   <div className={s.preloader}>
     <LogoIcon className={s.logo} />
-    {/* Replace with your LogoIcon or CSS Spinner */}
   </div>
 );
